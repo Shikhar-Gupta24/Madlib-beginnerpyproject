5445ac73bd1b37c1787b088c41d1486da0c16b1c
Shikhar-Gupta24 <80974447+Shikhar-Gupta24@users.noreply.github.com>
2021-04-17 22:42:11 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+Unedited = f"I like (hobby) because it is (adjective) I like to (verb1) because it is great. " \
+         f"I met (famous person) on the way to the mall. A(n) (animal) ran into my car and sadly died."
+print(Unedited)
+print("The above sentence is unedited please edit it the according way for your madlib")
+adj = input("Adjective:")
+verb1 = input("Verb:")
+famous_person = input("famous person:")
+Animal = input ("Animal:")
+hobby = input ("Hobby:")
+Madlib = f"I like {hobby} because it is {adj} I like to {verb1} because it is great. " \
+         f"I met {famous_person} on the way to the mall. A(n) {Animal} ran into my car and sadly died."
+print(Madlib)
+
